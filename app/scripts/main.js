b8605cf1b328b8e82a737f57de8736786082d611
Hongxu Liu <djshu.us@gmail.com>
2014-06-07 13:59:58 -0700

    Base repo for new backbone project

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+window.TodoMvc = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    TodoMvc.init();
+});
